3c246a8e808c9111a184ba8d2f091366940eac3f
Pablo <pablo.insua.gerpe@gmail.com>
2024-10-18 13:08:34 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import PageLoader from './components/PageLoader.vue';
+</script>
+
+<template>
+  <PageLoader />
+  <div id="container" class="w-screen h-screen bg-orange-400 text-white text-9xl justify-center items-center flex">
+    <h1>Ramon Insua</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/PageLoader.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="page-loader" class="page-loader" v-if="!isLoaded">
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+        <div class="ball"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => {
+            return {
+                isLoaded: false
+            }
+        },
+        mounted()  {
+            setTimeout(() => {
+                this.isLoaded = true;
+            }, 5000)
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $colors : #ffe0b5, #ffd191, #ffc26c, #ffb444;
+
+    .page-loader {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: #2b2b2b;
+        z-index: 999;
+        gap: 1em;
+        transition: 0.5s;
+    }
+
+    .ball {
+        width: 80px;
+        height: 80px;
+        border-radius: 100%;
+
+        @for $i from 1 through length($colors) {
+            &:nth-child(#{$i}) {
+                background-color: nth($colors, $i );
+            }
+        }
+        animation: wave 1.5s ease-in-out infinite;
+
+        &:nth-child(2) {
+            animation-delay: -0.2s;
+        }
+        &:nth-child(3) {
+            animation-delay: -0.4s;
+        }
+        &:nth-child(4) {
+            animation-delay: -0.6s;
+        }
+    }
+
+        @keyframes wave {
+            0%, 100% {
+                transform: translateY(30px);
+            }
+            50% {
+                transform: translateY(-30px);
+            }
+        }
+
+</style>
